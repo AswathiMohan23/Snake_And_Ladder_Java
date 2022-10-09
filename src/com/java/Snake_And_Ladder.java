9a972459947e20ca8dd9add3f364363264d4567c
AswathiMohan23 <achu999here@gmail.com>
2022-10-09 20:55:59 +0530

    [ADDED] : code for single player , starting point=0

---
+++ src/com/java/Snake_And_Ladder.java
@@ -0,0 +1,10 @@
+package com.java;
+
+//UC1 : single player,starting position = 0
+
+public class Snake_And_Ladder {
+    public static void main(String[] args){
+        System.out.println("Starting position = 0");
+    }
+
+}
