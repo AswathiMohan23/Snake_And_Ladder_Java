27ca4a53aae6dd8dc9bcbca3cd9f03beaed457a8
AswathiMohan23 <achu999here@gmail.com>
2022-10-09 20:58:34 +0530

    [ADDED] : code in which player rolls the die to get a number between 1 to 6

--- src/com/java/Snake_And_Ladder.java
+++ src/com/java/Snake_And_Ladder.java
@@ -1,10 +1,14 @@
 package com.java;
 
-//UC1 : single player,starting position = 0
+//UC2 : The Player rolls the die to get a number between 1 to 6.
+// Use ((RANDOM)) to get the number between 1 to 6
 
 public class Snake_And_Ladder {
     public static void main(String[] args){
+        int max=6,min=1;
+        int randomCheck= (int)(Math.random()*(max-min+1)+min); // get values between 1 to 6
         System.out.println("Starting position = 0");
+        System.out.println("player got : "+randomCheck);
     }
 
 }
